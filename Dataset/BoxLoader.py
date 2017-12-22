7d1e932dc8cd78759a32ca2df32b5a0adefc0a39
bindung <bindung@gmail.com>
2017-12-22 18:38:56 +0900

    Fix and apply icdar2013 text detection

--- Dataset/BoxLoader.py
+++ Dataset/BoxLoader.py
@@ -15,101 +15,101 @@
 # ==============================================================================
 
 
-import Dataset.coco.pycocotools.coco as coco
 import random
 import tensorflow as tf
 import threading
 import time
 import numpy as np
 
+
 class BoxLoader:
-	QUEUE_CAPACITY=16
+    QUEUE_CAPACITY = 16
 
-	def __init__(self, sources=[], initOnStart=True):
-		self.totalCount = 0
-		self.counts=[]
-		self.sources=[]
-		self.initDone=False
-		self.initOnStart=initOnStart
+    def __init__(self, sources=[], initOnStart=True):
+        self.totalCount = 0
+        self.counts = []
+        self.sources = []
+        self.initDone = False
+        self.initOnStart = initOnStart
 
-		with tf.name_scope('dataset') as scope:
-			self.queue = tf.FIFOQueue(dtypes=[tf.float32, tf.float32, tf.uint8],
-					capacity=self.QUEUE_CAPACITY)
+        with tf.name_scope('dataset') as scope:
+            self.queue = tf.FIFOQueue(dtypes=[tf.float32, tf.float32, tf.uint8],
+                                      capacity=self.QUEUE_CAPACITY)
 
-			self.image = tf.placeholder(dtype=tf.float32, shape=[None, None, 3], name="image")
-			self.boxes = tf.placeholder(dtype=tf.float32, shape=[None,4], name="boxes")
-			self.classes = tf.placeholder(dtype=tf.uint8, shape=[None], name="classes")
-			
-			self.enqueueOp = self.queue.enqueue([self.image, self.boxes, self.classes])
+            self.image = tf.placeholder(dtype=tf.float32, shape=[None, None, 3], name="image")
+            self.boxes = tf.placeholder(dtype=tf.float32, shape=[None, 4], name="boxes")
+            self.classes = tf.placeholder(dtype=tf.uint8, shape=[None], name="classes")
 
-		self.sources=sources[:]
-	
-	def categoryCount(self):
-		return 80
-	
-	def threadFn(self, tid, sess):
-		if tid==0:
-			self.init()
-		else:
-			while not self.initDone:
-				time.sleep(1)
+            self.enqueueOp = self.queue.enqueue([self.image, self.boxes, self.classes])
 
-		while True:
-			img, boxes, classes=self.selectSource().load()
-			try:
-				sess.run(self.enqueueOp,feed_dict={self.image:img, self.boxes:boxes, self.classes:classes})
-			except tf.errors.CancelledError:
-				return
+        self.sources = sources[:]
 
-	def init(self):
-		if not self.initOnStart:
-			for s in self.sources:
-				s.init()
+    def categoryCount(self):
+        return 1
 
-		for s in self.sources:
-			c = s.count()
-			self.counts.append(c)
-			self.totalCount+=c
+    def threadFn(self, tid, sess):
+        if tid == 0:
+            self.init()
+        else:
+            while not self.initDone:
+                time.sleep(1)
 
-		print("BoxLoader: Loaded %d files." % self.totalCount)
-		self.initDone=True
+        while True:
+            img, boxes, classes = self.selectSource().load()
+            try:
+                sess.run(self.enqueueOp, feed_dict={self.image: img, self.boxes: boxes, self.classes: classes})
+            except tf.errors.CancelledError:
+                return
 
-	def startThreads(self, sess, nThreads=4):
-		self.threads=[]
-		for n in range(nThreads):
-			t=threading.Thread(target=self.threadFn, args=(n,sess))
-			t.daemon = True
-			t.start()
-			self.threads.append(t)
+    def init(self):
+        if not self.initOnStart:
+            for s in self.sources:
+                s.init()
 
-	def get(self):
-		images, boxes, classes = self.queue.dequeue()
-		images = tf.expand_dims(images, axis=0)
+        for s in self.sources:
+            c = s.count()
+            self.counts.append(c)
+            self.totalCount += c
 
-		images.set_shape([None, None, None, 3])
-		boxes.set_shape([None,4])
+        print("BoxLoader: Loaded %d files." % self.totalCount)
+        self.initDone = True
 
-		return images, boxes, classes
+    def startThreads(self, sess, nThreads=4):
+        self.threads = []
+        for n in range(nThreads):
+            t = threading.Thread(target=self.threadFn, args=(n, sess))
+            t.daemon = True
+            t.start()
+            self.threads.append(t)
 
-	def add(self, source):
-		assert self.initDone==False
-		if self.initOnStart:
-			source.init()
-		self.sources.append(source)
+    def get(self):
+        images, boxes, classes = self.queue.dequeue()
+        images = tf.expand_dims(images, axis=0)
 
-	def selectSource(self):
-		i = random.randint(0, self.totalCount-1)
-		acc = 0
-		for j in range(len(self.counts)):
-			acc += self.counts[j]
-			if acc >= i:
-				return self.sources[j]
+        images.set_shape([None, None, None, 3])
+        boxes.set_shape([None, 4])
 
-	def count(self):
-		return self.totalCount
+        return images, boxes, classes
 
-	def getCaptions(self, categories):
-		return self.sources[0].getCaptions(categories)
+    def add(self, source):
+        assert self.initDone == False
+        if self.initOnStart:
+            source.init()
+        self.sources.append(source)
 
-	def getCaptionMap(self):
-		return self.getCaptions(np.arange(0,self.categoryCount()))
+    def selectSource(self):
+        i = random.randint(0, self.totalCount - 1)
+        acc = 0
+        for j in range(len(self.counts)):
+            acc += self.counts[j]
+            if acc >= i:
+                return self.sources[j]
+
+    def count(self):
+        return self.totalCount
+
+    def getCaptions(self, categories):
+        return self.sources[0].getCaptions(categories)
+
+    def getCaptionMap(self):
+        return self.getCaptions(np.arange(0, self.categoryCount()))
--- test.py
+++ test.py
@@ -25,6 +25,7 @@ from BoxInceptionResnet import BoxInceptionResnet
 from Visualize import Visualize
 from Utils import CheckpointLoader
 from Utils import PreviewIO
+import sys
 
 parser = argparse.ArgumentParser(description="RFCN tester")
 parser.add_argument('-gpu', type=str, default="0", help='Train on this GPU(s)')
@@ -33,17 +34,23 @@ parser.add_argument('-i', type=str, help='Input file.')
 parser.add_argument('-o', type=str, default="", help='Write output here.')
 parser.add_argument('-p', type=int, default=1, help='Show preview')
 parser.add_argument('-threshold', type=float, default=0.5, help='Detection threshold')
-parser.add_argument('-delay', type=int, default=-1, help='Delay between frames in visualization. -1 for automatic, 0 for wait for keypress.')
+parser.add_argument('-delay', type=int, default=-1,
+                    help='Delay between frames in visualization. -1 for automatic, 0 for wait for keypress.')
 
-opt=parser.parse_args()
+opt = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
 
-categories = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter',
- 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
- 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
- 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
- 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
- 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+categories = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+              'fire hydrant', 'stop sign', 'parking meter',
+              'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+              'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
+              'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+              'surfboard', 'tennis racket', 'bottle', 'wine glass',
+              'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+              'hot dog', 'pizza', 'donut', 'cake', 'chair',
+              'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+              'cell phone', 'microwave', 'oven', 'toaster',
+              'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
 palette = Visualize.Palette(len(categories))
 
@@ -52,51 +59,53 @@ net = BoxInceptionResnet(image, len(categories), name="boxnet")
 
 boxes, scores, classes = net.getBoxes(scoreThreshold=opt.threshold)
 
-
 input = PreviewIO.PreviewInput(opt.i)
 output = PreviewIO.PreviewOutput(opt.o, input.getFps())
 
+
 def preprocessInput(img):
-	def calcPad(size):
-		m = size % 32
-		p = int(m/2)
-		s = size - m
-		return s,p
+    def calcPad(size):
+        m = size % 32
+        p = int(m / 2)
+        s = size - m
+        return s, p
 
-	zoom = max(640.0 / img.shape[0], 640.0 / img.shape[1])
-	img = cv2.resize(img, (int(zoom*img.shape[1]), int(zoom*img.shape[0])))
+    zoom = max(640.0 / img.shape[0], 640.0 / img.shape[1])
+    img = cv2.resize(img, (int(zoom * img.shape[1]), int(zoom * img.shape[0])))
 
-	if img.shape[0] % 32 != 0:
-		s,p = calcPad(img.shape[0])
-		img = img[p:p+s]
+    if img.shape[0] % 32 != 0:
+        s, p = calcPad(img.shape[0])
+        img = img[p:p + s]
 
-	if img.shape[1] % 32 != 0:
-		s,p = calcPad(img.shape[1])
-		img = img[:,p:p+s]
+    if img.shape[1] % 32 != 0:
+        s, p = calcPad(img.shape[1])
+        img = img[:, p:p + s]
 
-	return img
+    return img
+
 
 with tf.Session() as sess:
-	if not CheckpointLoader.loadCheckpoint(sess, None, opt.n, ignoreVarsInFileNotInSess=True):
-		print("Failed to load network.")
-		sys.exit(-1)
+    if not CheckpointLoader.loadCheckpoint(sess, None, opt.n, ignoreVarsInFileNotInSess=True):
+        print("Failed to load network.")
+        sys.exit(-1)
 
-	while True:
-		img = input.get()
-		if img is None:
-			break
+    while True:
+        img = input.get()
+        if img is None:
+            break
 
-		img = preprocessInput(img)	
+        img = preprocessInput(img)
 
-		rBoxes, rScores, rClasses = sess.run([boxes, scores, classes], feed_dict={image: np.expand_dims(img, 0)})
+        rBoxes, rScores, rClasses = sess.run([boxes, scores, classes], feed_dict={image: np.expand_dims(img, 0)})
 
-		res = Visualize.drawBoxes(img, rBoxes, rClasses, [categories[i] for i in rClasses.tolist()], palette, scores=rScores)
+        res = Visualize.drawBoxes(img, rBoxes, rClasses, [categories[i] for i in rClasses.tolist()], palette,
+                                  scores=rScores)
 
-		output.put(input.getName(), res)
+        output.put(input.getName(), res)
 
-		if opt.p==1:
-			cv2.imshow("result", res)
-			if opt.o=="":
-				cv2.waitKey(input.getDelay() if opt.delay <0 else opt.delay)
-			else:
-				cv2.waitKey(1)
+        if opt.p == 1:
+            cv2.imshow("result", res)
+            if opt.o == "":
+                cv2.waitKey(input.getDelay() if opt.delay < 0 else opt.delay)
+            else:
+                cv2.waitKey(1)
