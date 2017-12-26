5a49928a1fcc2aad53230068f22d2a6ae7823f1f
root <root@ip-172-31-25-110.ec2.internal>
2017-12-26 16:27:13 +0900

    modify eval.sh

--- eval.sh
+++ eval.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-python3 eval.py -p=0 -i=/mnt/ICDAR2013/test/image -p=1 -n=/mnt/rfcn/test_icdar2013_test_no_aug/save/model-10000 -o=res
-cd res
-zip res.zip *.txt
-cd ../eval
-mv ../res/res.zip .
-python script.py -g=gt.zip -s=res.zip
+
+SAVEDIR=/root/result/rfcn/result/icdar_2013_2017/save
+
+for F in $(cd $SAVEDIR; ls model*.index) 
+do 
+    F=$(echo $F | awk -F '.' '{print $1}')
+    echo evaluation $F
+    rm -rf /tmp/res
+    mkdir /tmp/res
+    python3 eval.py -p=0 -i=/root/result/rfcn/data/ICDAR2013/test/image -n=$SAVEDIR/$F -o=/tmp/res
+    pushd /tmp/res
+    zip res.zip *.txt
+    popd
+    mv /tmp/res/res.zip eval/res20132017_$F.zip
+done
 
 
